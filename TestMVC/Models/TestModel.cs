b6f8207c7672b6328ab9cfc5ef2a89cf8b85640d
Adi <theadibest2@gmail.com>
2023-11-08 09:48:32 +0200

    Add new Test Controller

---
+++ TestMVC/Models/TestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TestMVC.Models
+{
+    public class TestModel
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+
+    }
+}
